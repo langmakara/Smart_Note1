3dc3de09007f76788e336868d9c2518ca935cb9f
lang makara <141546506+langmakara@users.noreply.github.com>
2025-12-23 20:21:22 +0700

    Fix indentation in reply function

--- app.py
+++ app.py
@@ -4,6 +4,6 @@ bot = telebot.TeleBot("8129717125:AAGU_LOnMWI70DS3gosTdwe3sFdwvkiUbio") # Replac
 
 @bot.message_handler(func=lambda _: True)
 def reply(message):
-bot.reply_to(message, "I'm alive 24/7! g7")
+  bot.reply_to(message, "I'm alive 24/7! g7")
 
 bot.polling(none_stop=True) # Keeps the bot running
