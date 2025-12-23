b9fa7f2a7c474efd83ed1f935014623846f43805
lang makara <141546506+langmakara@users.noreply.github.com>
2025-12-23 20:25:31 +0700

    Change bot token and correct indentation
    Updated bot token and fixed indentation in reply function.

--- app.py
+++ app.py
@@ -1,9 +1,11 @@
 import telebot
 
-bot = telebot.TeleBot("8129717125:AAGU_LOnMWI70DS3gosTdwe3sFdwvkiUbio") # Replace with your token
+# WARNING: Revoke this token in Telegram via @BotFather and get a new one!
+bot = telebot.TeleBot("8263985956:AAGC_XOIuLidE72XBhfcC_aBzWwN5IyfFYk") 
 
 @bot.message_handler(func=lambda _: True)
 def reply(message):
-  bot.reply_to(message, "I'm alive 24/7! g7")
+    # This line is now indented by 4 spaces
+    bot.reply_to(message, "I'm alive 24/7! g7")
 
-bot.polling(none_stop=True) # Keeps the bot running
+bot.polling(none_stop=True)
